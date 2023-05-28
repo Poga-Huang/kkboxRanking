32a73ae837e573696eb6fa99d872abfc826f5537
poga <s15114263@stu.edu.tw>
2023-05-28 13:23:34 +0800

    Initial Commit

---
+++ kkboxRanking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kkboxRanking
+//
+//  Created by 黃柏嘉 on 2023/5/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
